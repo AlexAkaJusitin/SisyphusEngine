c0aa7f195299285ef1004e3cef5996ec6c2fd741
Diaconescu Andrei-Alexandru <77116491+AlexAkaJusitin@users.noreply.github.com>
2024-08-26 18:19:16 +0300

    added camera

--- SisyphusEngine/shaderClass.cpp
+++ SisyphusEngine/shaderClass.cpp
@@ -1,6 +1,7 @@
 #include "shaderClass.h"
+#include <iostream>
 std::string get_file_contents(const char* filename)
-{
+{//this just reads the contents from the shader folder
 	std::ifstream in(filename, std::ios::binary);
 	if(in)
 	{
@@ -25,16 +26,20 @@ Shader::Shader(const char* vertexFile, const char* fragmentFile)
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexSource, NULL);
 	glCompileShader(vertexShader);
-
+	//get compile errors
+	compileErrors(vertexShader, "VERTEX");
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
 	glCompileShader(fragmentShader);
+	//get compile errors
+	compileErrors(fragmentShader, "FRAGMENT");
 
 	ID = glCreateProgram();
 	glAttachShader(ID, vertexShader);
 	glAttachShader(ID, fragmentShader);
 	glLinkProgram(ID);
-
+	//get linking errors
+	compileErrors(ID, "PROGRAM");
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 }
@@ -46,4 +51,28 @@ void Shader::Activate()
 void Shader::Delete()
 {
 	glDeleteProgram(ID);
+}
+
+void Shader::compileErrors(unsigned int shader, const char* type)
+{
+	GLint hasCompiled;
+	char infoLog[1024];
+	if (type != "PROGRAM")
+	{
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &hasCompiled);
+		if (hasCompiled == GL_FALSE)
+		{
+			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "SHADER_COMPILATION_ERROR for: " << type << "\n" << infoLog << "\n";
+		}
+	}
+	else
+	{
+		glGetProgramiv(shader, GL_LINK_STATUS, &hasCompiled);
+		if (hasCompiled == GL_FALSE)
+		{
+			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "PROGRAM_LINKING_ERROR for: " << type << "\n" << infoLog << "\n";
+		}
+	}
 }
--- SisyphusEngine/shaderClass.h
+++ SisyphusEngine/shaderClass.h
@@ -2,10 +2,10 @@
 #define SHADER_CLASS_H
 
 #include <glad\glad.h>
-#include<string>
-#include<fstream>
+#include <string>
+#include <fstream>
 #include <sstream>
-#include<cerrno>
+#include <cerrno>
 
 std::string get_file_contents(const char* filename);
 
@@ -16,6 +16,8 @@ public:
 	Shader(const char* vertexFile, const char* fragmentFile);
 	void Activate();
 	void Delete();
+private:
+	void compileErrors(unsigned int shader, const char* type);
 };
 
 
